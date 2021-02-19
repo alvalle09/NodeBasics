cad471270d96ecb8037685e57e869399e89bb0dd
alvalle09 <phredvalle@gmail.com>
2021-02-18 23:17:22 -0600

    setup api routes

--- app.js
+++ app.js
@@ -3,5 +3,15 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    
-})
+    res.send('Hello Vatos!@');
+});
+
+app.get('/api/courses', (req, res) => {
+    res.send([1,2,334]);
+});
+
+app.listen(3500, () => console.log('Escuch?'));
+
+
+
+
